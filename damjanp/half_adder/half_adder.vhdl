0ff0a08590d120ed7c9d91058851ecf406796ec8
damjan <damjanovmail@gmail.com>
2023-03-26 16:54:24 +0200

    .

--- damjanp/half_adder/half_adder.vhdl
+++ damjanp/half_adder/half_adder.vhdl
@@ -5,15 +5,14 @@ use ieee.std_logic_1164.all;
 
 entity half_adder is
 port(
-    A : in std_logic;
-    B : in std_logic;
-    S : out std_logic;
-    C : out std_logic
+    A: in std_logic;
+    B: in std_logic;
+    S: out std_logic;
+    C: out std_logic
 );
 end half_adder;
 
 architecture half_adder_functionality of half_adder is
-
 begin
 S <= A xor B;
 C <= A and B;
--- damjanp/half_adder/half_adder_tb.vhdl
+++ damjanp/half_adder/half_adder_tb.vhdl
@@ -1,5 +1,4 @@
 
-
 library ieee;
 
 use ieee.std_logic_1164.all;
@@ -10,20 +9,21 @@ end half_adder_tb;
 architecture half_adder_tb_functionality of half_adder_tb is
 component half_adder is
 port(
-    A : in std_logic;
-    B : in std_logic;
-    S : out std_logic;
-    C : out std_logic
+    A: in std_logic;
+    B: in std_logic;
+    S: out std_logic;
+    C: out std_logic
 );
 end component;
+
 signal input : std_logic_vector(1 downto 0);
 signal output : std_logic_vector(1 downto 0);
 begin
     hac: half_adder port map(
-       A => input(0),
-       B => input(1),
-       S => output(0),
-       C => output(1) 
+        A => input(0),
+        B => input(1),
+        S => output(0),
+        C => output(1)
     );
 
     testbench_process: process
@@ -32,7 +32,7 @@ begin
         input <= "01"; wait for 10 ns; assert output="01" report "greska 0+1 ne valja" severity error;
         input <= "10"; wait for 10 ns; assert output="01" report "greska 1+0 ne valja" severity error;
         input <= "11"; wait for 10 ns; assert output="10" report "greska 1+1 ne valja" severity error;
-        report "Half_adder testbench uspjesno zavrsen";
+        report "half_adder testbench je uspjesno izvrsen";
         wait;
     end process;
 end;
